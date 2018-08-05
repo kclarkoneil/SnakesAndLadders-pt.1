547d4a70c7d4ac51db7c7aff0bcef0e79d4f05f0
HighGroundLover <kclarkoneil@yahoo.ca>
2018-08-05 15:29:49 -0700

    Initial Commit

---
+++ Snakes and Ladders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Snakes and Ladders
+//
+//  Created by Kit Clark-O'Neil on 2018-08-05.
+//  Copyright © 2018 Kit Clark-O'Neil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
